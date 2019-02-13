64695986654e7faf5836914d7570a53553bc0dff
luominmin <luominmin@qtrade.com.cn>
2019-02-13 15:01:41 +0800

    国际化

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
-#### 介绍
+### 介绍
 cli工具 运行命令行自动生成react或者vue脚手架
 
-#### 使用
+### 使用
 * 全局安装 `npm i adomin-cli -g`
-* 新建目录 cd到目录下 `mkdir yourpro && cd yourpro`
-* 生成react脚手架 `adomin-cli init react` **当前目录必须是空目录**
-* 生成vue脚手架 `adomin-cli init vue` **当前目录必须是空目录**
+* 生成react脚手架 `adomin-cli init react projectName`
+* 生成vue脚手架 `adomin-cli init vue projectName`
 
-#### description
+### Description
 CLI for scaffolding vue or react projects
 
-#### Usage
+### Usage
 * Global installation `npm i adomin-cli -g`
-* Create a new folder and cd to this folder `mkdir yourpro && cd yourpro`
-* Create react scaffolding `adomin-cli init react` **The current folder must be empty**
-* Create vue scaffolding `adomin-cli init vue` **The current folder must be empty**
+* Create react scaffolding `adomin-cli init react projectName`
+* Create vue scaffolding `adomin-cli init vue projectName`
--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const chalk = require('chalk')
 const download = require('download-git-repo')
 const pkg = require('./package.json')
 const Spinner = require('cli-spinner').Spinner
-const spinner = new Spinner('下载中.. %s')
+const spinner = new Spinner('downloading... %s')
+const inquirer = require('inquirer');
+const fs = require('fs-extra')
 const url = {
   // gitlab私有库
   // const url = 'direct:http://192.xxx.0.xxx:8888/luominmin/caizhi-admin.git#qa'
@@ -12,26 +14,54 @@ const url = {
   vue: 'direct:https://github.com/myadomin/vue-temp.git'
 }
 
+const log = (str) => console.log(str)
+const logSuccess = (str) => console.log(chalk.green(str))
+const logError = (str) => console.log(chalk.red(str))
+
+const downloadFile = (projectType, projectName, needExample) => {
+  log(`download ${projectType} temp, to folder ${__dirname + '\\' + projectName}`)
+  spinner.start()
+  download(url[projectType], projectName, { clone: true }, function (err) {
+    if (err) {
+      logError('download error')
+    } else {
+      logSuccess('download success')
+    }
+    spinner.stop()
+  })
+}
 program
 .version(pkg.version, '-v, --version')
-.command('init [projectType]')
-.action(function(projectType, options){
-  if (projectType === 'react' || projectType === 'vue') {
-    const toPath = './'
-    console.log(`开始下载${projectType}模板`)
-    spinner.start()
-    download(url[projectType], toPath, { clone: true }, function (err) {
-      if (err) {
-        console.log(chalk.red('\n下载失败: 当前目录必须是空目录\n'), err)
+.command('init [projectType] [projectName]')
+.action(function(projectType, projectName, options){
+  if ((projectType === 'react' || projectType === 'vue') && projectName) {
+    fs.pathExists(__dirname + '\\' + projectName).then(exists => {
+      if (!exists) {
+        downloadFile(projectType, projectName)
+        // TODO 根据answers.example下载不同模板
+        // inquirer.prompt([
+        //   {
+        //     type: 'list',
+        //     message: '脚手架中是否需要example:',
+        //     name: 'example',
+        //     choices: [
+        //       "no",
+        //       "yes"
+        //     ],
+        //   }
+        // ]).then(answers => {
+        //   downloadFile(projectType, projectName, answers.example)
+        // });
       } else {
-        console.log(chalk.green('\n下载成功'))
+        logError('folder existed')
       }
-      spinner.stop()
+    }).catch(err => {
+      log(err)
     })
   } else {
-    console.log('请选择初始化react项目还是vue项目')
-    console.log('react项目请用命令：' + chalk.green('adomin-cli init react'))
-    console.log('vue项目请用命令：' + chalk.green('adomin-cli init vue'))
+    logError('input error')
+    log('react project input: adomin-cli init react projectName')
+    log('vue project input: adomin-cli init vue projectName')
   }
 })
 program.parse(process.argv)
